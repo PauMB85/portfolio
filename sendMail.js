7bad4209b56d1f3b44effae2fea621184baba42b
PauMB <paumb85@gmail.com>
2020-01-28 18:30:03 +0100

    se modifica aws lambda

--- sendMail.js
+++ sendMail.js
@@ -23,11 +23,11 @@ export function handler(event, context, callback){
     };
     return ses.sendEmail(params).promise().then(data => {
         console.log('mail_ok:',data);
-        return response(200, 'data: mail send ok');
+        return response(200, {data: 'mail send ok'});
       })
       .catch(err => {
         console.log('mail_ko:',err);
-        return response(500, 'error: error to send the mail');
+        return response(500, {error: 'error to send the mail'});
       });
 };
 
@@ -35,9 +35,9 @@ function response (statusCode, body){
     return {
         statusCode,
         headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Credentials": true
-        },
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': true,
+          },
         body: JSON.stringify(body)
     };
 }
