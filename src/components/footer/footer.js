cc42412a0339a5f142ae562419e425b20590e606
PauMB <paumb85@gmail.com>
2021-08-31 16:49:42 +0200

    test

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { Paper, IconButton } from '@material-ui/core';
-import { LinkedinLogo, GithubLogo } from './../../utils/LogosSVG';
+import { LinkedinLogo, GithubLogo } from '../../utils/LogosSVG';
 
 import './Footer.scss';
 
