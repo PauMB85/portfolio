2416d298b58421c1ee6fd3f19c47b17d170d5e6f
PauMB <paumb85@gmail.com>
2022-08-17 20:17:06 +0200

    Updates

---
+++ precache-manifest.625cce53ea6207dd8ed124485592cfe4.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0ea50dd053ce2857f72adecd10d532e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "da69696465dc9edecf61",
+    "url": "/portfolio/static/css/10.58c8b794.chunk.css"
+  },
+  {
+    "revision": "a1193add774a0f50da98",
+    "url": "/portfolio/static/css/11.65337719.chunk.css"
+  },
+  {
+    "revision": "326eb4515da39273aaf7",
+    "url": "/portfolio/static/css/6.d342a8d4.chunk.css"
+  },
+  {
+    "revision": "c0ea6e11e037763cd970",
+    "url": "/portfolio/static/css/7.b455b494.chunk.css"
+  },
+  {
+    "revision": "ff20e4ba8bca4591f929",
+    "url": "/portfolio/static/css/8.5555a729.chunk.css"
+  },
+  {
+    "revision": "a99c2550c0bdd11940b4",
+    "url": "/portfolio/static/css/9.5c566e01.chunk.css"
+  },
+  {
+    "revision": "f7362275ca5b4bee13ef",
+    "url": "/portfolio/static/css/main.78b14abb.chunk.css"
+  },
+  {
+    "revision": "b8e4d3ddbb4e957b8289",
+    "url": "/portfolio/static/js/0.4f7531ff.chunk.js"
+  },
+  {
+    "revision": "3fc7a8afc337456e6eb73316df3df069",
+    "url": "/portfolio/static/js/0.4f7531ff.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6c9b815376977b5f1fe9",
+    "url": "/portfolio/static/js/1.d51bdb59.chunk.js"
+  },
+  {
+    "revision": "da69696465dc9edecf61",
+    "url": "/portfolio/static/js/10.da54e2f9.chunk.js"
+  },
+  {
+    "revision": "a1193add774a0f50da98",
+    "url": "/portfolio/static/js/11.0e9068aa.chunk.js"
+  },
+  {
+    "revision": "5ddc3a05f4c308a6b50b",
+    "url": "/portfolio/static/js/4.30f6a300.chunk.js"
+  },
+  {
+    "revision": "f93e5d60b1eaa3e1e1d4",
+    "url": "/portfolio/static/js/5.d6946632.chunk.js"
+  },
+  {
+    "revision": "6ed4837c9741c870d03db3db11c80b52",
+    "url": "/portfolio/static/js/5.d6946632.chunk.js.LICENSE"
+  },
+  {
+    "revision": "326eb4515da39273aaf7",
+    "url": "/portfolio/static/js/6.7355f2c1.chunk.js"
+  },
+  {
+    "revision": "3fc7a8afc337456e6eb73316df3df069",
+    "url": "/portfolio/static/js/6.7355f2c1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c0ea6e11e037763cd970",
+    "url": "/portfolio/static/js/7.893e7b5b.chunk.js"
+  },
+  {
+    "revision": "ff20e4ba8bca4591f929",
+    "url": "/portfolio/static/js/8.a018adb7.chunk.js"
+  },
+  {
+    "revision": "a99c2550c0bdd11940b4",
+    "url": "/portfolio/static/js/9.8378b87e.chunk.js"
+  },
+  {
+    "revision": "f7362275ca5b4bee13ef",
+    "url": "/portfolio/static/js/main.7e58bec4.chunk.js"
+  },
+  {
+    "revision": "ac9539bff12b67710a3a",
+    "url": "/portfolio/static/js/runtime-main.ac0f0de3.js"
+  },
+  {
+    "revision": "797df12b3d0e7bda98b86a928e2cefb1",
+    "url": "/portfolio/static/media/CSS3_logo_and_wordmark.797df12b.svg"
+  },
+  {
+    "revision": "9a3f9ddde0669ecb8781778e6132e7fc",
+    "url": "/portfolio/static/media/HTML5_logo_resized.9a3f9ddd.svg"
+  },
+  {
+    "revision": "39a9ff933d94d6aa0392afec48382763",
+    "url": "/portfolio/static/media/angular-logo.39a9ff93.svg"
+  },
+  {
+    "revision": "205bbd743591616fb7bee195df4f5117",
+    "url": "/portfolio/static/media/aws-api-gateway.205bbd74.svg"
+  },
+  {
+    "revision": "a10058c5e18ccb3be1b6219113dcf9cc",
+    "url": "/portfolio/static/media/aws-dynamodb.a10058c5.svg"
+  },
+  {
+    "revision": "c485684571dcc4ecb405c70a4f656869",
+    "url": "/portfolio/static/media/aws-lambda.c4856845.svg"
+  },
+  {
+    "revision": "9897bacc56534804a00e62b57a2daa49",
+    "url": "/portfolio/static/media/estrella.9897bacc.webp"
+  },
+  {
+    "revision": "d93f1f5e46510348c2a0fcc4a0b199dd",
+    "url": "/portfolio/static/media/github-1.d93f1f5e.svg"
+  },
+  {
+    "revision": "ef9da5bd827675df148e5ad9c2f01761",
+    "url": "/portfolio/static/media/jslogo.ef9da5bd.svg"
+  },
+  {
+    "revision": "7650c2b0a606d7f26265318d2c55dac1",
+    "url": "/portfolio/static/media/linkedin-icon-2.7650c2b0.svg"
+  },
+  {
+    "revision": "2bf1064c4b139b4121f89765247b1edc",
+    "url": "/portfolio/static/media/me.2bf1064c.webp"
+  },
+  {
+    "revision": "d026d053735da2a7a47c121287fbd851",
+    "url": "/portfolio/static/media/me.d026d053.jpg"
+  },
+  {
+    "revision": "625e08a0f57066e8b3e0933967559d93",
+    "url": "/portfolio/static/media/nodejs-1.625e08a0.svg"
+  },
+  {
+    "revision": "30f9026e00b1ca7022f4d55fcdb8d3e3",
+    "url": "/portfolio/static/media/nombre-logo-pau.30f9026e.svg"
+  },
+  {
+    "revision": "be671209d888b03b8342298c13d2f51a",
+    "url": "/portfolio/static/media/react.be671209.svg"
+  },
+  {
+    "revision": "9db2a05c8598e30427dab351b8f0001d",
+    "url": "/portfolio/static/media/sass-1.9db2a05c.svg"
+  },
+  {
+    "revision": "5d3531a58c549105b96d5842a1e61b34",
+    "url": "/portfolio/static/media/spring-3.5d3531a5.svg"
+  },
+  {
+    "revision": "d9999a694038d4a06fa2809df84f2a44",
+    "url": "/portfolio/static/media/travis-ci.d9999a69.svg"
+  }
+]);
